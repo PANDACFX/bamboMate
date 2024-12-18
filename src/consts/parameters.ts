88ced1b28afbb9be674333867332e619f1813a15
Jhassiel Alcarraz <villenah03@gmail.com>
2024-12-18 01:14:59 +0000

    comit1

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,19 +1,21 @@
 /** Change these values to configure the application for your own use. **/
 
+import { ConfluxEspace } from "@thirdweb-dev/chains";
+
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0x1a44a37B5537d39588936acff6913E8d25a6FaaF";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
-export const chainConst = "mumbai";
+export const chainConst = ConfluxEspace
 
 // It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
-
+export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "e7c513a51c3bcd08867fd88554d0d441";
+//peque
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "blue";
 
